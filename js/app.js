d0d8f3247e56fb88dfc995de310f63727f81188c
Simone-Domenici <domenici.simone00@gmail.com>
2024-10-22 17:19:31 +0200

    ADD another version of the code adding a class

--- js/app.js
+++ js/app.js
@@ -11,12 +11,26 @@ const myButton = document.getElementById('my-button')
 //     }
 // })
 
+// myButton.addEventListener('click', () => {
+//     if(myButton.textContent.includes('Accendi')) {
+//         myButton.textContent = 'Spegni';
+//         lamp.src = './img/yellow_lamp.png';
+//     } else if (myButton.textContent.includes('Spegni')){
+//         myButton.textContent = 'Accendi';
+//         lamp.src = './img/white_lamp.png';
+//     }
+// })
+
 myButton.addEventListener('click', () => {
-    if(myButton.textContent.includes('Accendi')) {
+    if(myButton.classList.contains('power-off')) {
         myButton.textContent = 'Spegni';
         lamp.src = './img/yellow_lamp.png';
-    } else if (myButton.textContent.includes('Spegni')){
+        myButton.classList.toggle('power-off')
+        myButton.classList.toggle('power-on')
+    } else if (myButton.classList.contains('power-on')){
         myButton.textContent = 'Accendi';
         lamp.src = './img/white_lamp.png';
+        myButton.classList.toggle('power-off')
+        myButton.classList.toggle('power-on')
     }
 })
